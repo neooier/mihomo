c17ca35922345a4d06acfea03ab01bf00684d762
Jorylee <1747358809@qq.com>
2024-12-25 22:56:33 +0800

    fix(transport): 修复 icmp 监听器创建错误
    - 将 ICMP 监听器的协议从 "ip6:ipv6-icmp" 更改为 "udp6:ipv6-icmp"
    - 此修改解决了在某些系统上可能发生的权限错误问题

--- transport/tuic/icmp/listener.go
+++ transport/tuic/icmp/listener.go
@@ -10,7 +10,7 @@ import (
 
 func newICMPListener(address string) (net.PacketConn, error) {
 
-	conn, err := icmp.ListenPacket("ip6:ipv6-icmp", address)
+	conn, err := icmp.ListenPacket("udp6", address)
 	if err != nil {
 		return nil, err
 	}
