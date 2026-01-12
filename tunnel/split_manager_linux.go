35c6c87ee4cc2754a7de68bd042a94be04c77c14
Jory Li <53945377+neooier@users.noreply.github.com>
2026-01-12 15:32:26 +0800

    Add DHCP lease renewal for split interfaces

--- tunnel/split_manager_linux.go
+++ tunnel/split_manager_linux.go
@@ -28,6 +28,9 @@ type splitManager struct {
 type splitEntry struct {
 	ifaceName string
 	expires   time.Time
+	lastIPNet *net.IPNet
+	router    net.IP
+	cancel    context.CancelFunc
 }
 
 var globalSplitManager = &splitManager{
@@ -56,9 +59,14 @@ func (m *splitManager) getOrCreate(srcIP netip.Addr) (string, error) {
 	m.mu.Lock()
 	defer m.mu.Unlock()
 
-	if entry, ok := m.entries[srcIP]; ok && time.Until(entry.expires) > 30*time.Second {
-		log.Debugln("[SPLIT] reuse interface %s for %s", entry.ifaceName, srcIP)
-		return entry.ifaceName, nil
+	if entry, ok := m.entries[srcIP]; ok {
+		if time.Until(entry.expires) > 30*time.Second {
+			log.Debugln("[SPLIT] reuse interface %s for %s", entry.ifaceName, srcIP)
+			return entry.ifaceName, nil
+		}
+		if entry.cancel != nil {
+			entry.cancel()
+		}
 	}
 
 	if m.parentIface == "" {
@@ -86,59 +94,150 @@ func (m *splitManager) getOrCreate(srcIP netip.Addr) (string, error) {
 		return "", fmt.Errorf("set macvlan up: %w", err)
 	}
 
-	if err := m.acquireDHCP(ifaceName, macvlan); err != nil {
+	lease, client, err := m.acquireDHCP(ifaceName, macvlan, nil)
+	if err != nil {
 		return "", err
 	}
 
+	leaseTime := lease.ACK.IPAddressLeaseTime(time.Hour)
+	renewTime := lease.ACK.IPAddressRenewalTime(leaseTime / 2)
+	leaseCtx, cancel := context.WithCancel(context.Background())
 	m.entries[srcIP] = &splitEntry{
 		ifaceName: ifaceName,
-		expires:   time.Now().Add(time.Hour),
+		expires:   time.Now().Add(leaseTime),
+		lastIPNet: leaseIPNet(lease),
+		router:    leaseRouter(lease),
+		cancel:    cancel,
 	}
+	go m.renewLeaseLoop(leaseCtx, srcIP, ifaceName, client, lease, renewTime, leaseTime)
 	log.Debugln("[SPLIT] assigned interface %s for %s", ifaceName, srcIP)
 	return ifaceName, nil
 }
 
-func (m *splitManager) acquireDHCP(ifaceName string, link netlink.Link) error {
+func (m *splitManager) acquireDHCP(ifaceName string, link netlink.Link, oldIPNet *net.IPNet) (*nclient4.Lease, *nclient4.Client, error) {
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel()
 
 	client, err := nclient4.New(ifaceName, nclient4.WithTimeout(5*time.Second), nclient4.WithRetry(1))
 	if err != nil {
-		return fmt.Errorf("init dhcp client: %w", err)
+		return nil, nil, fmt.Errorf("init dhcp client: %w", err)
 	}
 	lease, err := client.Request(ctx)
 	if err != nil {
-		return fmt.Errorf("dhcp request on %s: %w", ifaceName, err)
+		return nil, nil, fmt.Errorf("dhcp request on %s: %w", ifaceName, err)
 	}
 
-	ip := lease.ACK.YourIPAddr
-	mask := lease.ACK.SubnetMask()
-	if ip == nil || mask == nil {
+	if err := m.applyLease(link, ifaceName, lease, oldIPNet, nil); err != nil {
+		return nil, nil, err
+	}
+
+	return lease, client, nil
+}
+
+func (m *splitManager) applyLease(link netlink.Link, ifaceName string, lease *nclient4.Lease, oldIPNet *net.IPNet, oldRouter net.IP) error {
+	ipnet := leaseIPNet(lease)
+	if ipnet == nil {
 		return fmt.Errorf("dhcp lease missing ip or mask")
 	}
-
-	addr := &netlink.Addr{
-		IPNet: &net.IPNet{
-			IP:   ip.Mask(mask),
-			Mask: mask,
-		},
+	if oldIPNet != nil && !oldIPNet.IP.Equal(ipnet.IP) {
+		_ = netlink.AddrDel(link, &netlink.Addr{IPNet: oldIPNet})
 	}
+	addr := &netlink.Addr{IPNet: ipnet}
 	if err := netlink.AddrAdd(link, addr); err != nil && !isExists(err) {
 		return fmt.Errorf("assign address to %s: %w", ifaceName, err)
 	}
 
-	routers := lease.ACK.Router()
-	if len(routers) > 0 {
+	router := leaseRouter(lease)
+	if len(oldRouter) > 0 && router != nil && !oldRouter.Equal(router) {
+		_ = netlink.RouteDel(&netlink.Route{
+			LinkIndex: link.Attrs().Index,
+			Gw:        oldRouter,
+		})
+	}
+	if router != nil {
 		route := &netlink.Route{
 			LinkIndex: link.Attrs().Index,
-			Gw:        routers[0],
+			Gw:        router,
 		}
 		if err := netlink.RouteAdd(route); err != nil && !isExists(err) {
 			return fmt.Errorf("add default route on %s: %w", ifaceName, err)
 		}
 	}
-	log.Infoln("[SPLIT] macvlan %s leased %s via %v", ifaceName, ip, routers)
+	log.Infoln("[SPLIT] macvlan %s leased %s via %v", ifaceName, ipnet.IP, router)
 	return nil
+}
+
+func (m *splitManager) renewLeaseLoop(ctx context.Context, srcIP netip.Addr, ifaceName string, client *nclient4.Client, lease *nclient4.Lease, renewTime time.Duration, leaseTime time.Duration) {
+	for {
+		wait := renewTime
+		if wait <= 0 {
+			wait = leaseTime / 2
+		}
+		if wait <= 0 {
+			wait = 30 * time.Minute
+		}
+		select {
+		case <-time.After(wait):
+		case <-ctx.Done():
+			return
+		}
+
+		link, err := netlink.LinkByName(ifaceName)
+		if err != nil {
+			log.Warnln("[SPLIT] renew link %s for %s failed: %v", ifaceName, srcIP, err)
+			continue
+		}
+
+		renewCtx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+		newLease, err := client.Renew(renewCtx, lease)
+		cancel()
+		if err != nil {
+			log.Warnln("[SPLIT] renew lease on %s failed: %v", ifaceName, err)
+			newLease, client, err = m.acquireDHCP(ifaceName, link, leaseIPNet(lease))
+			if err != nil {
+				log.Warnln("[SPLIT] re-request lease on %s failed: %v", ifaceName, err)
+				continue
+			}
+		} else {
+			if err := m.applyLease(link, ifaceName, newLease, leaseIPNet(lease), leaseRouter(lease)); err != nil {
+				log.Warnln("[SPLIT] apply renewed lease on %s failed: %v", ifaceName, err)
+				continue
+			}
+		}
+
+		lease = newLease
+		leaseTime = lease.ACK.IPAddressLeaseTime(leaseTime)
+		renewTime = lease.ACK.IPAddressRenewalTime(leaseTime / 2)
+
+		m.mu.Lock()
+		if entry, ok := m.entries[srcIP]; ok {
+			entry.expires = time.Now().Add(leaseTime)
+			entry.lastIPNet = leaseIPNet(lease)
+			entry.router = leaseRouter(lease)
+		}
+		m.mu.Unlock()
+		log.Debugln("[SPLIT] renewed lease on %s for %s", ifaceName, srcIP)
+	}
+}
+
+func leaseIPNet(lease *nclient4.Lease) *net.IPNet {
+	ip := lease.ACK.YourIPAddr
+	mask := lease.ACK.SubnetMask()
+	if ip == nil || mask == nil {
+		return nil
+	}
+	return &net.IPNet{
+		IP:   ip.Mask(mask),
+		Mask: mask,
+	}
+}
+
+func leaseRouter(lease *nclient4.Lease) net.IP {
+	routers := lease.ACK.Router()
+	if len(routers) == 0 {
+		return nil
+	}
+	return routers[0]
 }
 
 func isExists(err error) bool {
